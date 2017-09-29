303013c1624739e70bdf3b37b40a8f6faefe342a
seaofocean <xianyan.jia@intel.com>
2017-09-29 14:06:14 +0800

    update

---
+++ pipeline/ssd/src/main/java/pipeline/ssd/caffe/Combine2.java
@@ -0,0 +1,7 @@
+package pipeline.ssd.caffe;
+
+/**
+ * Created by jxy on 9/28/17.
+ */
+public class Combine2 {
+}
---
+++ pipeline/ssd/src/test/scala/com/intel/analytics/zoo/pipeline/common/Test/scala.scala
@@ -0,0 +1,8 @@
+package com.intel.analytics.zoo.pipeline.common.Test
+
+/**
+ * Created by jxy on 9/28/17.
+ */
+class scala {
+
+}
